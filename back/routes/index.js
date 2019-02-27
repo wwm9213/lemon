366091f5ef91f0427ce22c40adb57f23f002a2a4
wwm <wwm9213@qq.com>
2019-02-27 19:38:48 +0800

    后台端口完成

--- back/routes/index.js
+++ back/routes/index.js
@@ -4,6 +4,8 @@ var mongod = require('mongodb-curd');
 var db = 'myLemon';
 var userCol = 'user'; //用户表
 var billCol = 'bill'; //账单表
+var classifyCol = 'classify'; //分类图标表
+var iconCol = 'icon';
 
 /* 注册 */
 router.post('/api/userlist', function(req, res, next) {
@@ -89,6 +91,61 @@ router.post('/api/delBill', function(req, res, next) {
             res.json({ code: 0, msg: '删除失败' });
         } else {
             res.json({ code: 1, msg: '删除成功' });
+        }
+    })
+})
+
+//添加分类
+router.post('/api/addClass', function(req, res, next) {
+    var parms = req.body,
+        uid = parms.uid,
+        type = parms.type,
+        icon = parms.icon,
+        intro = parms.intro;
+    mongod.insert(db, classifyCol, { 'uid': uid, 'type': type, 'icon': icon, 'intro': intro }, function(result) {
+        if (!result) {
+            res.json({ code: 0, msg: '添加失败' });
+        } else {
+            res.json({ code: 1, msg: '添加成功' });
+        }
+    })
+})
+
+//获取分类图标
+router.post('/api/getClassify', function(req, res, next) {
+    var parms = req.body,
+        uid = parms.uid,
+        type = parms.type;
+    mongod.find(db, classifyCol, { 'uid': uid, 'type': type }, function(result) {
+        if (!result) {
+            res.json({ code: 0, msg: '没有数据' });
+        } else {
+            res.json({ code: 1, msg: '查找成功', data: result });
+        }
+    })
+})
+
+//添加icon图标
+router.post('/api/addIcon', function(req, res, next) {
+    var parms = req.body,
+        icon = prams.icon
+    iconName = parms.iconName;
+    mongod.insert(db, iconCol, { 'icon': icon, 'iconName': iconName }, function(result) {
+        if (!result) {
+            res.json({ code: 0, msg: '添加失败' });
+        } else {
+            res.json({ code: 1, msg: '添加成功', data: result });
+        }
+    })
+})
+
+//获取所有icon图标
+router.get('/api/getIcon ', function(req, res, next) {
+    mongod.insert(db, iconCol, function(result) {
+        if (!result) {
+            res.json({ code: 0, msg: '没有数据' });
+        } else {
+            res.json({ code: 1, msg: '获取成功', data: result });
         }
     })
 })
